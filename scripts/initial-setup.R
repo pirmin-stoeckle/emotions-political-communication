0de9fb45a35def988d91b3e30ccfd8ea767f977c
Pirmin Stöckle <57797210+pirmin-stoeckle@users.noreply.github.com>
2021-05-24 18:27:34 +0200

    Initial commit

---
+++ scripts/initial-setup.R
@@ -0,0 +1,18 @@
+update.packages(ask = FALSE, checkBuilt = TRUE)
+tinytex::tlmgr_update()
+
+library(here)
+library(fs)
+
+dir_create(here("data", "raw"))        # raw data
+dir_create(here("data", "processed"))  # intermediate data
+dir_create(here(c("scripts",           # storing all .R, .Rmd, .py files
+                  "figures",           # output figures
+                  "output",            # output files
+                  "previous",          # previous versions keep for record
+                  "paper",             # current versions of the document
+                  "notes",             # random notes
+                  "documents"          # supporting documents
+)
+)
+)
