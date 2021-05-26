9299d723b7407884bdc8d9f3a5dd160dfd59b802
Pirmin Stoeckle <pstoeckl@ad.uni-mannheim.de>
2021-05-26 19:01:09 +0200

    analysis

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -5,23 +5,58 @@ source("scripts/data-preparation.R")
 library(lubridate)
 
 data %>% 
-  group_by(`Page Name`) %>% 
+  group_by(party) %>% 
   summarize(sum_interactions = sum(`Total Interactions`)) %>% 
   arrange(desc(sum_interactions))
 
+data %>% 
+  ggplot(aes(x = `Total Interactions`)) +
+  geom_histogram(bins = 100) +
+  facet_wrap(~party)
+
+
+data %>%
+  filter(date > "2017-09-24") %>% 
+  mutate(date_week = round_date(date, unit = "week")) %>% 
+  group_by(party, date_week) %>% 
+  summarize(sum_interactions = sum(`Total Interactions`)) %>% 
+  ggplot(aes(x = date_week, y = sum_interactions, group = party)) +
+  geom_point(aes(color = party)) + 
+  geom_smooth() +
+  facet_wrap(~party)
 
 data %>% 
   mutate(year_month = format(data$`Post Created Date`, "%Y-%m")) %>% 
   filter(year_month > "2017-01") %>% 
-  group_by(`Page Name`, year_month) %>% 
+  group_by(party, year_month) %>% 
   summarize(sum_interactions = sum(`Total Interactions`)) %>% 
-  ggplot(aes(x = year_month, y = sum_interactions, group = `Page Name`)) +
-    geom_point(aes(color = `Page Name`)) + 
+  ggplot(aes(x = year_month, y = sum_interactions, group = party)) +
+    geom_point(aes(color = party)) + 
     geom_smooth() +
-    facet_wrap(~`Page Name`)
+    facet_wrap(~party)
 
 head(format(data$`Post Created Date`, "%Y-%m"))
 
+names(data)
+data %>% 
+  filter(year_month > "2017-01") %>% 
+  group_by(party, year_month) %>% 
+  summarize(n = n(),
+            interactions = sum(`Total Interactions`),
+            love = sum(Love),
+            angry = sum(Angry),
+            shares = sum(Shares),
+            interaction_per_post = interactions/n,
+            love_per_post = love/n) %>% 
+  ggplot(aes(x = date, y = n, group = party)) +
+  geom_point(aes(color = party)) + 
+  geom_smooth() +
+  facet_wrap(~party)
+
+ggplot(data, aes(x = date, y = `Total Interactions`, group = party)) +
+  geom_point(aes(color = party)) + 
+  geom_smooth() +
+  facet_wrap(~party)
 
 
 head(data$Message)
--- scripts/data-preparation.R
+++ scripts/data-preparation.R
@@ -7,4 +7,17 @@ data <- read_csv("data/raw/2021-05-17-10-59-15-CEST-Historical-Report-German-Par
 
 glimpse(data)
 
+# put "Partei" und "Fraktion" in ein label: "party"
+data$party <- fct_collapse(data$`Page Name`,
+             cdu_csu = c("CDU", "CDU/CSU-Bundestagsfraktion"),
+             spd = c("SPD", "SPD-Fraktion im Bundestag"),
+             gruene = c("Bündnis 90/Die Grünen Bundestagsfraktion", "BÜNDNIS 90/DIE GRÜNEN"),
+             fdp = c("FDP", "FDP Fraktion Bundestag"),
+             linke = c("DIE LINKE", "Fraktion DIE LINKE. im Bundestag"),
+             afd = c("AfD", "AfD-Fraktion im Deutschen Bundestag"))
+# date
+
+data <- data %>% 
+  mutate(date = data$`Post Created Date`,
+         year_month = format(data$`Post Created Date`, "%Y-%m"))
 
