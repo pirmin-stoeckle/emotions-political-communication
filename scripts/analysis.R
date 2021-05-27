df0b100411106f84e37ca594683405c08a11d901
Pirmin Stoeckle <pstoeckl@ad.uni-mannheim.de>
2021-05-27 17:54:41 +0200

    new stuff trying out

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -3,67 +3,91 @@ source("scripts/data-preparation.R")
 
 
 library(lubridate)
+# set standard theme
+theme_set(theme_minimal())
 
+# total interactions by party
 data %>% 
   group_by(party) %>% 
-  summarize(sum_interactions = sum(`Total Interactions`)) %>% 
+  summarize(n_posts = n(),
+            sum_interactions = sum(total_interactions)) %>% 
   arrange(desc(sum_interactions))
 
-data %>% 
-  ggplot(aes(x = `Total Interactions`)) +
-  geom_histogram(bins = 100) +
-  facet_wrap(~party)
 
-
+# interactions by party over time
 data %>%
-  filter(date > "2017-09-24") %>% 
-  mutate(date_week = round_date(date, unit = "week")) %>% 
-  group_by(party, date_week) %>% 
-  summarize(sum_interactions = sum(`Total Interactions`)) %>% 
-  ggplot(aes(x = date_week, y = sum_interactions, group = party)) +
+  filter(date > "2017-01-01") %>% 
+  ggplot(aes(x = date, y = total_interactions, group = party)) +
   geom_point(aes(color = party)) + 
   geom_smooth() +
-  facet_wrap(~party)
+  facet_wrap(~party) 
 
+
+# when was the introduction of reactions? (2016)
 data %>% 
-  mutate(year_month = format(data$`Post Created Date`, "%Y-%m")) %>% 
-  filter(year_month > "2017-01") %>% 
-  group_by(party, year_month) %>% 
-  summarize(sum_interactions = sum(`Total Interactions`)) %>% 
-  ggplot(aes(x = year_month, y = sum_interactions, group = party)) +
-    geom_point(aes(color = party)) + 
-    geom_smooth() +
-    facet_wrap(~party)
+  filter(Angry > 0) %>% 
+  arrange(date) %>% 
+  select(Message, party, Angry, date) %>% 
+  head()
 
-head(format(data$`Post Created Date`, "%Y-%m"))
+ggplot(data[data$date > "2017-01-01",], aes(x = date, y = Angry)) +
+  geom_point()
 
-names(data)
+# angry by party over time
+data %>%
+  filter(date > "2017-01-01") %>% 
+  group_by(party) %>% 
+  ggplot(aes(x = date, y = angry_share, group = party)) +
+  geom_point(aes(color = party), alpha = 0.2) + 
+  geom_smooth() +
+  #geom_vline(xintercept = date("2017-09-24")) + # BTW
+  #geom_vline(xintercept = date("2020-03-19")) + # Merkels Anpsrache
+  facet_wrap(~party)
+
+# was ist da bei der spd los
 data %>% 
-  filter(year_month > "2017-01") %>% 
-  group_by(party, year_month) %>% 
-  summarize(n = n(),
-            interactions = sum(`Total Interactions`),
-            love = sum(Love),
-            angry = sum(Angry),
-            shares = sum(Shares),
-            interaction_per_post = interactions/n,
-            love_per_post = love/n) %>% 
-  ggplot(aes(x = date, y = n, group = party)) +
-  geom_point(aes(color = party)) + 
+  filter(party == "SPD") %>% 
+  arrange(desc(angry_share)) %>% 
+  select(date, Message, angry_share) %>% 
+  head(20)
+
+# love by party over time
+data %>%
+  filter(date > "2017-01-01") %>% 
+  group_by(party) %>% 
+  ggplot(aes(x = date, y = love_share, group = party)) +
+  geom_point(aes(color = party), alpha = 0.2) + 
   geom_smooth() +
   facet_wrap(~party)
 
-ggplot(data, aes(x = date, y = `Total Interactions`, group = party)) +
-  geom_point(aes(color = party)) + 
-  geom_smooth() +
-  facet_wrap(~party)
+# Since 2017, what is the distribution of love and share
+summary(data[data$date > "2017-01-01",]$Love)
+summary(data[data$date > "2017-01-01",]$Angry)
 
+table(data[data$date > "2017-01-01",]$Love>10)
+table(data[data$date > "2017-01-01",]$Angry>8)
 
-head(data$Message)
+table(data[data$date > "2017-01-01",]$Angry>10, data[data$date > "2017-01-01",]$Love>8)
+# -> these things could be predicted by a machine learning model from characteristics of the post
 
 library(tidytext)
 
 data_token <- data %>%
-  mutate(year_month = format(data$`Post Created Date`, "%Y-%m")) %>% 
   unnest_tokens(word, Message)
 head(data_token$word)
+
+# most successful post
+data %>% 
+  arrange(desc(total_interactions)) %>% 
+  select(party, total_interactions, Message) %>% 
+  head() %>% 
+  pull(Message)
+
+# post with most angry reactions
+data %>% 
+  arrange(desc(Angry)) %>% 
+  head() %>% 
+  pull(Message, Angry)
+
+
+table(str_detect(data$Message, "Corona"))
--- scripts/data-preparation.R
+++ scripts/data-preparation.R
@@ -1,23 +1,51 @@
 # load packages
 library(tidyverse)
-
+library(lubridate)
 # load data from crowdtangle (historical data so available as a download-link sent via email)
 
-data <- read_csv("data/raw/2021-05-17-10-59-15-CEST-Historical-Report-German-Parties-2004-02-03--2021-05-17.csv")
+data <- read_csv("data/raw/2021-05-27-14-03-55-CEST-Historical-Report-German-Political-Parties-1969-12-31--2021-05-27.csv")
 
 glimpse(data)
 
 # put "Partei" und "Fraktion" in ein label: "party"
-data$party <- fct_collapse(data$`Page Name`,
-             cdu_csu = c("CDU", "CDU/CSU-Bundestagsfraktion"),
-             spd = c("SPD", "SPD-Fraktion im Bundestag"),
-             gruene = c("Bündnis 90/Die Grünen Bundestagsfraktion", "BÜNDNIS 90/DIE GRÜNEN"),
-             fdp = c("FDP", "FDP Fraktion Bundestag"),
-             linke = c("DIE LINKE", "Fraktion DIE LINKE. im Bundestag"),
-             afd = c("AfD", "AfD-Fraktion im Deutschen Bundestag"))
-# date
+# data$party <- fct_collapse(data$`Page Name`,
+#              cdu_csu = c("CDU", "CDU/CSU-Bundestagsfraktion"),
+#              spd = c("SPD", "SPD-Fraktion im Bundestag"),
+#              gruene = c("Bündnis 90/Die Grünen Bundestagsfraktion", "BÜNDNIS 90/DIE GRÜNEN"),
+#              fdp = c("FDP", "FDP Fraktion Bundestag"),
+#              linke = c("DIE LINKE", "Fraktion DIE LINKE. im Bundestag"),
+#              afd = c("AfD", "AfD-Fraktion im Deutschen Bundestag"))
 
+data$party <- as.factor(data$`Page Name`)
+
+summary(data$party)
+
+# date
 data <- data %>% 
   mutate(date = data$`Post Created Date`,
-         year_month = format(data$`Post Created Date`, "%Y-%m"))
+         date_day = round_date(date, unit = "day"),
+         date_week = round_date(date, unit = "week"),
+         date_month = round_date(date, unit = "month"),
+         date_year = round_date(date, unit = "year"))
+summary(data$date_day)
+
+# interactions
+data$total_interactions <-  data$`Total Interactions`
+
+summary(data$total_interactions)
+
+# reactions as share on all interactions
+data <- data %>% 
+  mutate(reactions = Likes + Love + Wow + Haha + Sad + Angry + Care,
+         likes_share = Likes / reactions,
+         love_share = Love / reactions,
+         wow_share = Wow / reactions,
+         haha_share = Haha / reactions,
+         sad_share = Sad / reactions,
+         angry_share = Angry / reactions,
+         care_share = Care / reactions)
+
+data %>% 
+  select(reactions:care_share) %>% 
+  summary()
 
